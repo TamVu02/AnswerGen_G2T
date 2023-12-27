edbf2079bb92e4889d9ec0ffbb260a6cb6015a97
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2023-12-27 10:07:27 +0700

    Add files via upload

--- metric.py
+++ metric.py
@@ -1,12 +1,18 @@
+import evaluate
+from loguru import logger
+
+bleu = evaluate.load('bleu')
+
 def evaluate_bleu(data_ref, data_sys):
-    bleu_score=0
+    global bleu
+    bleu_score = 0
     for i in range(len(data_sys)):
         try:
-            output_metric = corpus_bleu([data_ref[i]],[data_sys[i]],smoothing_function=SmoothingFunction().method1)
+            output_metric = bleu.compute(predictions=data_sys[i], references=data_ref[i])
         except Exception as e:
-            print('Error in calculate bleu score')
-            output_metric = 0
-        bleu_score+=output_metric
+            logger.warning(e)
+            output_metric = {'bleu':0}
+        bleu_score+=output_metric['bleu']
         avg_bleu=bleu_score/len(data_sys)
 
     return avg_bleu
